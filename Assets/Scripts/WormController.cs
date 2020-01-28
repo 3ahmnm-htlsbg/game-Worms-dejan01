99e523e4de459a2e63cea7ddba98805afc61f966
Dejan01 <dejan.klickovic01@htl-salzburg.ac.at>
2020-01-28 15:09:21 +0100

    Add if statement and check if a key is pressed

--- Assets/Scripts/WormController.cs
+++ Assets/Scripts/WormController.cs
@@ -4,46 +4,11 @@ using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
-
-    public int x;
-    public string text;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        /*
-        if(x > 10)
+        if(Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("x ist größer als 10");
-
+            Debug.Log("Wird aufgerufen");
         }
-        else
-        {
-            Debug.Log("x ist kleiner als 10");
-        }
-
-        if(text == "DEJAN"){
-            Debug.Log("In der text Variable steht DEJAN");
-        }
-        else
-        {
-            Debug.Log("Es steht etwas anderes");
-        }
-        */
-
-        if(x>10){
-            Debug.Log("X ist größer oder gleich als 10");
-        }
-        else
-        {
-            Debug.Log("X ist kleiner oder gleich als 10");
-        }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
